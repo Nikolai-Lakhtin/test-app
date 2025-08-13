679906219e2768cdf3068d3ccdb5be34c1225e05
Nickolai <nick.lahtin@yandex.ru>
2025-08-13 11:05:57 +0300

    Blogpage Promise

--- src/pages/Blogpage.tsx
+++ src/pages/Blogpage.tsx
@@ -12,18 +12,30 @@ const blogList: BlogListType[] = [
   {title: 'state three', id: 3, text:'Lorem title, Lorem title, Lorem title'},
 ]
 
+const setBlogList = (): Promise<BlogListType[]> => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(blogList)
+      // reject(0)
+    }, 1000)
+  })
+}
+
 const Blogpage: React.FC = () => {
   const [list, setList] = useState<BlogListType[]| []>([])
+  const [isError, setIsError] = useState(false)
 
   useEffect(() => {
-    let time: ReturnType<typeof setTimeout>
     (async() => {
-      time = setTimeout(() => {
-        setList(blogList)
-      }, 1000)
-    })()
+      setIsError(false)
+      try{
+        const result = await setBlogList()
+        setList(result)
+      } catch {
+        setIsError(true)
+      }
 
-    return () => {clearTimeout(time)}
+    })()
   },[])
 
   const removeHandler = (id: number) : void => {
@@ -35,6 +47,7 @@ const Blogpage: React.FC = () => {
       <div>
         This is Blogpage
       </div>
+      {!isError ? (
       <ul>
         {list.map(({id, title, text}) => (
           <li key={id} onDoubleClick={() => removeHandler(id)}>
@@ -42,7 +55,10 @@ const Blogpage: React.FC = () => {
             <p>{text}</p>
           </li>
         ))}
-      </ul>
+      </ul>) : (
+        <p>This is Error</p>
+      )}
+
     </>
 
   )
